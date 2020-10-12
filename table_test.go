a92f01ce52f7613b9d8bedb2bec3de3db6c17c97
Oscar Franzén <oscar@jayloop.se>
2020-10-12 08:49:37 +0200

    changed test package from table to table_test

--- table_test.go
+++ table_test.go
@@ -1,13 +1,15 @@
-package table
+package table_test
 
 import (
 	"os"
 	"runtime"
 	"testing"
+
+	"github.com/jayloop/table"
 )
 
 func Example() {
-	t := New("key", "value")
+	t := table.New("key", "value")
 	t.Precision(2, 1)
 	t.Row("a", 1)
 	t.Row("b", 2.0)
@@ -22,10 +24,10 @@ func Example() {
 }
 
 func TestTable(t *testing.T) {
-	tbl := New("key", "value", "other")
-	tbl.FormatHeader(Format(HiYellow, Background(Black), Bold, Underline))
-	tbl.FormatCols(Format(White, Background(Blue)), 0)
-	tbl.FormatCols(Format(Background(Green)), 1)
+	tbl := table.New("key", "value", "other")
+	tbl.FormatHeader(table.Format(table.HiYellow, table.Background(table.Black), table.Bold, table.Underline))
+	tbl.FormatCols(table.Format(table.White, table.Background(table.Blue)), 0)
+	tbl.FormatCols(table.Format(table.Background(table.Green)), 1)
 	tbl.Precision(4, 1)
 	tbl.Precision(8, 2)
 	tbl.Row(int(1), float64(1), float64(2))
@@ -39,9 +41,9 @@ func TestTable(t *testing.T) {
 func TestTableFromStruct(t *testing.T) {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	tbl := New("key", "value")
-	tbl.FormatHeader(Format(HiYellow, Bold))
-	tbl.FormatCols(Format(HiCyan), 0)
+	tbl := table.New("key", "value")
+	tbl.FormatHeader(table.Format(table.HiYellow, table.Bold))
+	tbl.FormatCols(table.Format(table.HiCyan), 0)
 	tbl.Precision(4, 1)
 	tbl.MaxWidth(60, 1)
 	tbl.AddStruct(m)
